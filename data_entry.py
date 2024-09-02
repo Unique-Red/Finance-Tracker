fb42f15ed0b18c3391354ce0451f3d8098fc1655
unique-red <vnoah410@gmail.com>
2024-09-02 14:28:20 -0700

    added graph

--- data_entry.py
+++ data_entry.py
@@ -8,7 +8,7 @@ def get_date(prompt, allow_default=False):
     if allow_default and not date_str:
         return datetime.today().strftime(date_format)
     try:
-        valid_date = datetime.strftime(date_format)
+        valid_date = datetime.strptime(date_str, date_format)
         return valid_date.strftime(date_format)
     except ValueError:
         print("Invalid date format. Please enter the date in dd-mm-yyyy format")
